7e78b854e79edb04381d1a91c204504edbb4fab4
VintageSouLover <VintageSouLover>
2022-06-30 14:14:25 -0700

    Úsing printf to output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Print a string
+ * printf function is used to print the (“character, string, float, integer, octal and hexadecimal values”) onto the output screen
+ * Return: 0
+  */
+
+
+
+int main(void)
+
+{
+
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
