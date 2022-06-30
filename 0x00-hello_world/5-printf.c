eae1fe15788aed221cc2b76b9d8861c6a5feeb57
VintageSouLover <VintageSouLover>
2022-06-30 14:18:10 -0700

    using printf to output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
 
 /**
  * main - Print a string
- * printf function is used to print the (“character, string, float, integer, octal and hexadecimal values”) onto the output screen
+ * Printf is used to print the output
  * Return: 0
-  */
+ */
 
 
 
