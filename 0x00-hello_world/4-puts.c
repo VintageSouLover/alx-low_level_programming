bae7512e087e93c434f85987af4bc58b9b33f824
VintageSouLover <VintageSouLover>
2022-06-30 14:07:54 -0700

    not using printf to output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+nclude <stdio.h>
+
+
+
+/**
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
+ */
+
+
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
