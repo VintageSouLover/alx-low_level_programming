7cc501d9b965ff5f4179b32e6cdba5b589286b80
VintageSouLover <VintageSouLover>
2022-06-30 13:44:28 -0700

    Add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,14 @@
 
 
 /**
-
-  * main - Entry point
  
-  * Description: prints "\"Programming is like building a multilingual puzzle"
-
-  * Return: 0
-
-  */
+   * main - Entry point
+ 
+   * Description: prints "\"Programming is like building a multilingual puzzle"
+ 
+   * Return: 0
+ 
+   */
 
 
 
